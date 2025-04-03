d9c55d9c61daa1d059bd0566d3265d20201c6223
Gabriel Davanço <gabrieldavanco4@gmail.com>
2025-04-03 17:09:36 -0300

    Questão 2 concluida
    apk e zip em suas respectivas pastas

--- Questao2/app/src/main/java/br/com/fecapapp/percentualsalario/MainActivity.java
+++ Questao2/app/src/main/java/br/com/fecapapp/percentualsalario/MainActivity.java
@@ -1,6 +1,8 @@
 package br.com.fecapapp.percentualsalario;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -11,11 +13,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView txtResultado;
     private RadioGroup radioGroup;
     private RadioButton rb40, rb45, rb50;
+    private EditText etSalario;
+    private double doubleSalario;
 
 
     @Override
@@ -25,15 +31,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txtResultado = findViewById(R.id.txtResultado);
+        radioGroup = findViewById(R.id.rbGroup);
         rb40 = findViewById(R.id.rb40);
         rb45 = findViewById(R.id.rb45);
         rb50 = findViewById(R.id.rb50);
-        
+        etSalario = findViewById(R.id.editTxtSalario);
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void calcular(View view){
+        String strSalario = etSalario.getText().toString();
+        doubleSalario = Double.parseDouble(strSalario);
+
+        if(rb40.isChecked()) doubleSalario += doubleSalario*0.4;
+        if(rb45.isChecked()) doubleSalario += doubleSalario*0.45;
+        if(rb50.isChecked()) doubleSalario += doubleSalario*0.5;
+
+        DecimalFormat df = new DecimalFormat("#,##0.00");
+        strSalario = df.format(doubleSalario);
+        txtResultado.setText("R$ " + strSalario);
+
+    }
+
+    public void limpar(View view){
+        radioGroup.clearCheck();
+        etSalario.setText("");
+        txtResultado.setText("---");
     }
 }
