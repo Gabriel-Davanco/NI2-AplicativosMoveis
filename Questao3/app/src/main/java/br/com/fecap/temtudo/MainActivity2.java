5d156cc3724ace460d440913b03010200fef300f
Gabriel Davanço <gabrieldavanco4@gmail.com>
2025-04-09 16:51:56 -0300

    Questão 3 concluida
    zip e apk estão em suas devidas pastas também

---
+++ Questao3/app/src/main/java/br/com/fecap/temtudo/MainActivity2.java
@@ -0,0 +1,76 @@
+package br.com.fecap.temtudo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private EditText etNome;
+    private Button btnConcluir;
+    private String strNome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+
+        btnConcluir = findViewById(R.id.btnConcluir);
+        etNome = findViewById(R.id.etNome);
+
+        btnConcluir.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity3.class);
+            strNome = etNome.getText().toString();
+
+            intent.putExtra("nome", strNome);
+            startActivity(intent);
+            finish();
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+}
---
+++ Questao3/app/src/main/java/br/com/fecap/temtudo/MainActivity3.java
@@ -0,0 +1,45 @@
+package br.com.fecap.temtudo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    private TextView txtNome;
+    private Button btnVoltar;
+    private String nome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main3);
+        btnVoltar = findViewById(R.id.btnVoltar);
+        txtNome = findViewById(R.id.txtNome);
+
+        Bundle bundle = getIntent().getExtras();
+
+        nome = bundle.getString("nome");
+        txtNome.setText("Muito obrigado por concluir o cadastro " + nome + ", esperamos que aproveite de seus benefícios!");
+
+        btnVoltar.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
